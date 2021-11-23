b5a8200ee02567b18a7f42ecde6581fedaed324e
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-11-23 08:54:03 -0300

    Exibindo os types retornados da api

--- src/components/PokeCard/index.tsx
+++ src/components/PokeCard/index.tsx
@@ -1,9 +1,9 @@
 import pokeBg from '../../images/pokeBg.png';
-import pokeball from '../../images/pokeball.png';
 
 import { Container } from "./styles"
 import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
+
 
 interface PokemonTypesProps {
   name: string;
@@ -12,7 +12,7 @@ interface PokemonTypesProps {
 interface PokemonProps {
   id: number;
   image: string;
-  // types: PokemonTypesProps[];
+  types: PokemonTypesProps[];
 }
 
 export const PokeCard = ({ name }: any) => {
@@ -20,26 +20,28 @@ export const PokeCard = ({ name }: any) => {
   
   useEffect(() => {
     api.get(`pokemon/${name}`)
-    .then(response => {
+    .then( response => {
       const { id, types, sprites } = response.data;
       setPokemon({
         id,
         image: sprites.other['official-artwork'].front_default,
-        // types: types.map()
+        types: types.map((type: { type: any; }) => type.type.name),
       })
       
     }
     
-      
+    
      
   )}, [name]);
   
+  console.log(pokemon.types)
   return(
     <Container>
     <div className="cardInfo">
       <span>#{pokemon.id}</span>
       <h1>{name}</h1>
-
+        <p>{pokemon.types}</p>
+  
     </div>
       <div className="cardImgs">
         <img className="pokeBg" src={pokeBg} alt="" />
