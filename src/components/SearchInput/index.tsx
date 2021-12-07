7cd744de2d895dbf6f4aa01bfe822540ad603743
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-12-07 13:01:33 -0300

    Update no SearchInput Component

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -7,22 +7,19 @@ export function SearchInput() {
   const { SearchPokemon } = usePokemon();
 
   function handleSubmit({target}: ChangeEvent<HTMLInputElement>) {
-  /*  console.log( target.typeof()) */
     var targetValue = target.value;
     SearchPokemon(targetValue);
   }
 
   return(
-  /*   <form onSubmit={handleSubmit} >
-      <label htmlFor="searchPokemon"> */
+    
+      <div>
         <input
           type="text"
-          placeholder="Digite o nome de um pókemon"
-          
+          placeholder="Digite o nome de um pókemon" 
           onChange={handleSubmit}
         />
-/*         <img src={lupa} alt="Lupa" />
-      </label>
-    </form> */
+        <img src={lupa} alt="Lupa" />
+      </div>
   )
 }
